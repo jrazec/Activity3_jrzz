e58c00c938733d934ffb62cbfe05632ddc35903c
jrazec <johnrazecschool@gmail.com>
2023-11-05 11:43:13 +0800

    feat4: add another column item_price syntax

--- hero.sql
+++ hero.sql
@@ -154,3 +154,10 @@ VALUES
     (8, 8, 108),  -- Fanny has Thunder Belt
     (9, 9, 109),  -- Leomord has Blade Armor
     (10, 10, 110);  -- Chou has Feather of Heaven
+
+--3.    Create new branch named "feat/add-column-price"
+
+ALTER TABLE item 
+ADD item_price DECIMAL(6,2); 
+-- Used 6 digits since mostly, mlbb’s or any other MOBA games’ item
+--cost usually ranges from hundreds to thousands e.g(1234.10).
